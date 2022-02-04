c19ff47f1bef5dc4899ef1a0da86cb7ecf22c746
David Stockdale <dstock3@gmail.com>
2022-02-04 00:15:50 -0600

    Executed a series of map methods on the content object in order to render the content data.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,36 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import content from '../data/content.json'
 
 function Portfolio() {
-    const [categories, setCategories] = useState([])
-
-    useEffect(()=> {
-        let categoryArray = []
-        if (content) {
-            for (let prop in content) {
-                categoryArray.push(content[prop])
-            }
-        }
-        setCategories(categoryArray)
-    }, [])
-    console.log(categories)
-
-
   return (
     <section className="portfolio">
         {Object.keys(content).map((keyName, i) => (
-
-            <div className={keyName}>
+            <div className="category" id={keyName}>
                 <h3>{keyName}</h3>
-                {Object.keys(content[keyName]).map((article, i)=>(
-                    <article className={article}>
-                        <div>{article.title}</div>
-                    </article>
+                <div className="row">
+                    {Object.keys(content[keyName]).map((article, i)=>(
+                        <article className={article}>
+                            <h4>
+                                <a href={content[keyName][article].source} target="_blank" rel="noopener noreferrer">{content[keyName][article].title}</a>
+                            </h4>
+                            <p>{content[keyName][article].client}</p>
+                            {content[keyName][article].date ?
+                                <p>{content[keyName][article].date}</p> : null
+                            }
+                            <p>{content[keyName][article].desc}</p>
+                        </article>
 
-                ))}
+                    ))}
+                </div>
             </div>
         ))}
-
     </section>
     )
 }
