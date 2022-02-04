73ca9d02464ace425de124896f24e08baccf7839
David Stockdale <dstock3@gmail.com>
2022-02-03 23:48:46 -0600

    Added useEffect hook in Porfolio array to prepare the data.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import content from '../data/content.json'
 
 function Portfolio() {
+    const [categories, setCategories] = useState([])
+
+    useEffect(()=> {
+        let categoryArray = []
+        if (content) {
+            for (let prop in content) {
+                categoryArray.push(content[prop])
+            }
+        }
+        setCategories(categoryArray)
+    }, [])
+    console.log(categories)
+
+
   return (
     <section className="portfolio">
         {Object.keys(content).map((keyName, i) => (
