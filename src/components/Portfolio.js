bcfc48ba00ed840683d29618f6777a71073fe712
David Stockdale <dstock3@gmail.com>
2022-02-05 14:49:09 -0600

    Added select container

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,40 +2,45 @@ import React, {useState} from 'react';
 import { content } from '../data/content.js'
 import '../css/portfolio.css'
 
-
 function Portfolio(props) {
-    //const [select, setSelect] = useState(["Automotive Agency", "Content Marketing", "Personal Finance", "Music"])
+    const [select, setSelect] = useState(["Automotive Agency", "Content Marketing", "Personal Finance", "Music"])
+    const [contentSelection, setContentSelection] = useState(content)
 
     return (
         <section className="portfolio">
-            {Object.keys(content).map((category) => (
+            <div className="select-container">
+                {select.map((item)=> (
+                    <div className="select">{item}</div>
+                ))}
+            </div>
+            {Object.keys(contentSelection).map((category) => (
                 <div className="category">
                     <h3>{category}</h3>
                         <div className="row"  style={{ justifyContent: 'center'}}>
-                            {Object.keys(content[category]).map((article)=>(
+                            {Object.keys(contentSelection[category]).map((article)=>(
                                 <article className="writing" id={content[category][article].id}>
-                                    {content[category][article].img ? 
-                                        <a href={content[category][article].source} className="img-link" target="_blank" rel="noopener noreferrer">
+                                    {contentSelection[category][article].img ? 
+                                        <a href={contentSelection[category][article].source} className="img-link" target="_blank" rel="noopener noreferrer">
                                             <div className="article-image-container">
-                                                <div className="article-image" alt={content[category][article].img.alt} style={{ backgroundPosition: 'center', backgroundSize: 'cover', backgroundImage: `url(${content[category][article].img.source})`}}></div>
+                                                <div className="article-image" alt={contentSelection[category][article].img.alt} style={{ backgroundPosition: 'center', backgroundSize: 'cover', backgroundImage: `url(${contentSelection[category][article].img.source})`}}></div>
                                             </div> 
                                         </a> : null
                                     }
                                     <div className="client">
-                                        {content[category][article].client}
-                                        {content[category][article].date ?
-                                            <> | {content[category][article].date}</> : null
+                                        {contentSelection[category][article].client}
+                                        {contentSelection[category][article].date ?
+                                            <> | {contentSelection[category][article].date}</> : null
                                         }
                                     </div>
                                     <div className="dec"></div>
                                     <h4 className='article-title'>
-                                        <a href={content[category][article].source} className="article-link" target="_blank" rel="noopener noreferrer">{
-                                            content[category][article].title}
+                                        <a href={contentSelection[category][article].source} className="article-link" target="_blank" rel="noopener noreferrer">{
+                                            contentSelection[category][article].title}
                                         </a>
                                     </h4>
                                     
 
-                                    <p>{content[category][article].desc}</p>
+                                    <p>{contentSelection[category][article].desc}</p>
                                 </article>
                             ))}
                     </div>
